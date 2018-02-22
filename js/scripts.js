5b1ff14d25597749589526a24ca72f671ec972da
Lankiewicz <36169986+Lankiewicz@users.noreply.github.com>
2018-02-22 12:13:11 +0100

    test inkrementacja

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
-foo = 'lorem' + ['1', 2, 3.4];     // 'lorem1, 2, 3.4'
-foo = ['1', 2, 3.4] + 'lorem';     // '1,2,3.4lorem'
-foo = 9 + ['1', 2, 3.4];             // '91,2,3.4'
-foo = ['1', 2, 3.4] + 9;             // '1,2,3.49'
-foo = ['1', 2, 3.4] + [1, '2', 3.4];  // '1,2,3.41,2,3.4'
+var points = 1;
 
-foo = ['1', 2, 3.4] + {bar: 'lorem'}; // 1, 2, 3.4 [object Object]
-foo = {bar: 'lorem'} + [1, '2', 3.4]; // [object Object] 1, 2, 3.4
-
-foo = ['1', 2, 3.4] + undefined; // 1, 2, 3.4 undefined
-foo = undefined + [1, '2', 3.4]; // undefined 1, 2, 3.4
-
+console.log( 'The current value of points is: ', points ); // 1
+console.log( 'The current value of points is: ', points++ ); // 1
+console.log( 'The current value of points is: ', points ); // 2

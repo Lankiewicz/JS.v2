752a1029a0bd6d1faadaf248e8956f1eb364ba0d
Lankiewicz <36169986+Lankiewicz@users.noreply.github.com>
2018-02-22 12:04:36 +0100

    test add

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+foo = 'lorem' + ['1', 2, 3.4];     // 'lorem1, 2, 3.4'
+foo = ['1', 2, 3.4] + 'lorem';     // '1,2,3.4lorem'
+foo = 9 + ['1', 2, 3.4];             // '91,2,3.4'
+foo = ['1', 2, 3.4] + 9;             // '1,2,3.49'
+foo = ['1', 2, 3.4] + [1, '2', 3.4];  // '1,2,3.41,2,3.4'
+
+foo = ['1', 2, 3.4] + {bar: 'lorem'}; // 1, 2, 3.4 [object Object]
+foo = {bar: 'lorem'} + [1, '2', 3.4]; // [object Object] 1, 2, 3.4
+
+foo = ['1', 2, 3.4] + undefined; // 1, 2, 3.4 undefined
+foo = undefined + [1, '2', 3.4]; // undefined 1, 2, 3.4
 
